bba91e8f32bf2f367b743ea5cedfd40b908921a2
rutvijdave12 <62494387+rutvijdave12@users.noreply.github.com>
2020-04-03 19:03:51 +0530

    Update login.py

--- Readex/login.py
+++ Readex/login.py
@@ -1,6 +1,9 @@
-#'(UID BODY[1])'
-#'(RFC822)'
-#'(UID BODY[TEXT])'
+#login has a class called mail consisting of 3 functions.
+
+
+#'(UID BODY[1])'-----------------> used to get just the body of the email.
+#'(RFC822)'----------------------> gets you rhe whole email contents.
+#'(UID BODY[TEXT])'--------------> again gets you just the body of the email but with some changes(see for yourself).
 
 import imaplib
 import email
@@ -8,10 +11,14 @@ import config
 
 class mail:
 
+#login function helps you to login to your email by passing your email credentials.
+#This function is invoked in the driver script after instantiating the class mail.
     def login (self,UNM,PASS):
         self.msvr = imaplib.IMAP4_SSL('imap.gmail.com',993)
         self.msvr.login(UNM,PASS)
 
+#initiate will select the inbox of your email you can change it to other stuff by printing msvr.list() and typing one of the list contents...
+#instead of "Inbox"
     def initiate (self):
         self.msvr.select("Inbox")
         typ, data = self.msvr.search(None, 'ALL')
@@ -20,6 +27,7 @@ class mail:
         self.id_list = self.id_list[::-1]
         latest_email_id = int( self.id_list[0] )
 
+#retrieve uses a fetch method which will fetch you a particular email depending on the parameter passed in your driver script.
     def retrieve (self,num):
         status,data = self.msvr.fetch(self.id_list[num],'(RFC822)' )
         return data[0][1]
