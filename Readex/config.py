72993338bd57a894c1c8225c9831a7fc5cad86d3
rutvijdave12 <62494387+rutvijdave12@users.noreply.github.com>
2020-04-08 11:29:34 +0530

    Update config.py

--- Readex/config.py
+++ Readex/config.py
@@ -1,4 +1,4 @@
 #Enter your gmail credantials which shall be used to login using the driver script.
-SENDER_EMAIL_ADDRESS = 'minip2323@gmail.com'
-PASSWORD = 'Shaunak@1999'
+SENDER_EMAIL_ADDRESS = 'yourusername@gmail.com'
+PASSWORD = 'yourpassword'
 RECEIVER_EMAIL_ADDRESS = ''
