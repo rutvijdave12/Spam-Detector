806ee804b677111a7f995c9075e58bdc9d30f43d
rutvijdave12 <62494387+rutvijdave12@users.noreply.github.com>
2020-04-08 10:43:13 +0530

    Delete clean.py

--- Readex/clean.py
+++
@@ -1,7 +0,0 @@
-#Used to clean the content
-from bs4 import BeautifulSoup
-
-def clean(text):
-    cleantext = BeautifulSoup(text,"lxml").text
-    #lxml is a Python library which allows for easy handling of XML and HTML files, and can also be used for web scraping.
-    return cleantext
